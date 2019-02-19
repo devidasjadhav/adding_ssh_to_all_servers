f21813b111862064e50b84327ce54794c236cbbd
Devidas Jadhav <freekepange@gmail.com>
2019-02-19 16:24:05 +0530

    added sshpass
    added sshpass to aid multiple password entring

--- ssh_spread.sh
+++ ssh_spread.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+set -v
 
 #create key to passing it to all servers
 ssh-keygen -t rsa -b 4096 -C "Devidas@Jadhav.com" -f id_rsa -N ''
 
-for server_ip in $@ :
+for server_ip in $@
 do
-  ssh root@$server_ip -- mkdir -p .ssh
-  scp id_rsa.pub root@$server_ip:.ssh/authorized_keys
-  scp id_rsa root@$server_ip:.ssh/
-  ssh root@$server_ip --  "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
+  echo $server_ip
+  sshpass -f pass_file ssh root@$server_ip -- mkdir -p .ssh
+  sshpass -f pass_file scp id_rsa.pub root@$server_ip:.ssh/authorized_keys
+  sshpass -f pass_file scp id_rsa root@$server_ip:.ssh/
+  sshpass -f pass_file ssh root@$server_ip --  "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 done
